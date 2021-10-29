81fdfe3fbe5d22e629b77786d870d823c9b13306
Someone Obscure <someoneobscure0989@protonmail.com>
2021-10-30 10:01:57 +1100

    Add star rating interactions

--- app/javascript/packs/starrating.js
+++ app/javascript/packs/starrating.js
@@ -1,18 +1,60 @@
 const DATA_RATING = 'data-rating'
 const DATA_RATING_MAX = 'data-rating-max'
+const DATA_RATING_READONLY = 'data-rating-readonly'
+const DATA_RATING_CALLBACK = 'data-rating-callback'
 
 const STAR_EMPTY = 'star-empty'
 const STAR_FULL = 'star-full'
 
+const ZERO_AREA_PADDING_PX = 10
 
 class StarRating {
-  constructor(container, score, maxScore) {
+  constructor(container, score, maxScore, readonly, callback) {
     this.state = {
       container,
       score,
       maxScore,
+      readonly,
+      callback,
+      hoverScore: undefined,
     }
     this.initStars()
+  }
+
+  _getEventScore(e) {
+    const idx = Number(e.currentTarget.getAttribute('data-rating-idx'))
+    let x = e.offsetX
+
+    if (idx === 0) {
+      if (x < ZERO_AREA_PADDING_PX) {
+        return 0
+      }
+      x -= ZERO_AREA_PADDING_PX
+    }
+
+    return idx + 1
+  }
+
+  _handleMouseMove(e) {
+    const score = this._getEventScore(e)
+    if (score !== this.state.hoverScore) {
+      this.state.hoverScore = score
+      this.renderScore()
+    }
+  }
+
+  _handleMouseOut(e) {
+    this.state.hoverScore = undefined
+    this.renderScore()
+  }
+
+  _handleClick(e) {
+    const score = this._getEventScore(e)
+    this.state.score = score
+    const callback = window[this.state.callback]
+    if (typeof callback === 'function') {
+      callback(score)
+    }
   }
 
   initStars() {
@@ -23,8 +65,15 @@ class StarRating {
     for (let i = 0; i < this.state.maxScore; i++) {
       const starEl = starTemplate.cloneNode(true)
       starEl.setAttribute('data-rating-idx', i)
+      if (!this.state.readonly) {
+        starEl.addEventListener('mousemove', this._handleMouseMove.bind(this))
+        starEl.addEventListener('mouseout', this._handleMouseOut.bind(this))
+        starEl.addEventListener('click', this._handleClick.bind(this))
+      }
       this.state.container.appendChild(starEl)
     }
+
+    this.state.container.firstElementChild.style.paddingLeft = `${ZERO_AREA_PADDING_PX}px`
 
     this.renderScore()
   }
@@ -37,7 +86,14 @@ class StarRating {
   }
 
   renderScore() {
-    const { score, maxScore, container } = this.state
+    const {maxScore, container, readonly, hoverScore} = this.state
+
+    let score
+    if (!readonly && hoverScore !== undefined) {
+      score = hoverScore
+    } else {
+      score = this.state.score
+    }
 
     let fullStars = Math.max(Math.trunc(Math.min(score, maxScore)), 0)
     const stars = container.children
@@ -52,10 +108,11 @@ class StarRating {
   }
 }
 
-
 document.querySelectorAll('[role="star-rating"]').forEach((container) => {
   const score = Number(container.getAttribute(DATA_RATING))
   const maxScore = Number(container.getAttribute(DATA_RATING_MAX)) || 5
+  const readonly = container.getAttribute(DATA_RATING_READONLY) !== null
+  const callback = container.getAttribute(DATA_RATING_CALLBACK)
 
-  new StarRating(container, score, maxScore)
+  new StarRating(container, score, maxScore, readonly, callback)
 })
