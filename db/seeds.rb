4cef760069cf13188f76805450da9530f7573c89
Someone Obscure <someoneobscure0989@protonmail.com>
2021-10-29 22:20:20 +1100

    Seed records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+products = [
+  ['sample-product', 'The Sample Product'],
+  ['second-sample-product', 'Sample Product №2']
+]
+
+products.each do |slug, title|
+  Product.create(slug: slug, title: title)
+end
+
+
+reviews = [
+  [1, 5, 'Amazing! This must be one of my favourite products. I am using it all the time. Best buy in a long time.'],
+  [1, 3, 'Fair.'],
+  [1, 2, 'Wasn\'t really satisfied with it.'],
+  [2, 3, 'It\'s okay but it could use some improvements. Usability is lacking in some places. Overall decent product.'],
+  [2, 1, 'I regret ever looking at it.'],
+  [2, 4, 'Quite good!']
+]
+
+reviews.each do |product_id, score, text|
+  Review.create(product_id: product_id, score: score, text: text)
+end
