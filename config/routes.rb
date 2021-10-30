f0e8f4a7f42dc336be64e384693eda3ddaeb09a3
Someone Obscure <someoneobscure0989@protonmail.com>
2021-10-30 14:14:28 +1100

    Add submit_review on product controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/products/:slug', to: 'products#detail'
+  post '/products/submitReview', to: 'products#submit_review'
 end
