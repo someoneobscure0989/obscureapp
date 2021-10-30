e5ef5124bfb3377082546cdf67a95ff474b09b2a
Someone Obscure <someoneobscure0989@protonmail.com>
2021-10-30 14:26:18 +1100

    Add root route that redirects to sample product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/products/sample-product', status: 302)
+
   get '/products/:slug', to: 'products#detail'
   post '/products/submitReview', to: 'products#submit_review'
 end
