0ae642eea9b1663a0d9ca3c42bb6e2f2aef5d7b9
Someone Obscure <someoneobscure0989@protonmail.com>
2021-10-31 11:57:53 +1100

    Add controller test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: redirect('/products/sample-product', status: 302)
 
   get '/products/:slug', to: 'products#detail'
-  post '/products/submitReview', to: 'products#submit_review'
+  post '/products/submitReview', to: 'products#submit_review', as: 'submit_review'
 
   namespace :api do
     resources :products, param: :slug, only: [:show], defaults: { format: :json }
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get detail" do
-    get products_detail_url
-    assert_response :success
+  test "should_have_new_average_score" do
+    post submit_review_url, params: {
+      slug: products(:one).slug,
+      score: 4,
+      text: 'A test text'
+    }
+    puts products(:one).reviews.inspect
+    assert products(:one).average_score == 2.5
   end
 end
